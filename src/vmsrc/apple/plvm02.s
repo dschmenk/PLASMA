54a256cd60c6f2d14385d81a244529675a50dd83
David Schmenk <dschmenk@sbcglobal.net>
2024-01-03 07:05:46 -0800

    Add JUMP/JUMPZ opcodes to PLVM for PLFORTH

--- src/vmsrc/apple/plvm02.s
+++ src/vmsrc/apple/plvm02.s
@@ -144,7 +144,7 @@ OPTBL   !WORD   ZERO,CN,CN,CN,CN,CN,CN,CN                               ; 00 02 
         !WORD   NEG,COMP,BAND,IOR,XOR,SHL,SHR,IDXW                      ; 90 92 94 96 98 9A 9C 9E
         !WORD   BRGT,BRLT,INCBRLE,ADDBRLE,DECBRGE,SUBBRGE,BRAND,BROR    ; A0 A2 A4 A6 A8 AA AC AE
         !WORD   ADDLB,ADDLW,ADDAB,ADDAW,IDXLB,IDXLW,IDXAB,IDXAW         ; B0 B2 B4 B6 B8 BA BC BE
-        !WORD   NATV                                                    ; C0
+        !WORD   NATV,JUMPZ,JUMP                                         ; C0 C2 C4
 ;*
 ;* DIRECTLY ENTER INTO BYTECODE INTERPRETER
 ;*
@@ -1467,6 +1467,28 @@ NATV    TYA                     ; FLATTEN IP
         JMP     (IP)
 +       INC     IPH
         JMP     (IP)
+;*
+;* JUMPS FOR FORTH COMPILER
+;*
+JUMPZ   INX
+        LDA     ESTKH-1,X
+        ORA     ESTKL-1,X
+        BEQ     JUMP
+        INY                     ;+INC_IP
+        INY
+        BMI     +
+        JMP     NEXTOP
++       JMP     FIXNEXT
+JUMP    INY
+        LDA     (IP),Y
+        PHA
+        INY
+        LDA     (IP),Y
+        STA     IPH
+        PLA
+        STA     IPL
+        LDY     #$00
+        JMP     FETCHOP
 VMEND   =       *
 }
 ;************************************************
